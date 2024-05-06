43faf4db2c85eae5971e4acb1e1e0156962ecca4
Mark Raynsford <code@io7m.com>
2024-05-06 22:27:47 +0000

    Update workflows.

--- .github/workflows/deploy-snapshot.sh
+++ .github/workflows/deploy-snapshot.sh
@@ -21,6 +21,12 @@ fi
   xmlns="http://maven.apache.org/SETTINGS/1.0.0"
   xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
   xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
+
+  <!-- Enable deployment of OCI images. -->
+  <activeProfiles>
+    <activeProfile>io7m-oci-image</activeProfile>
+  </activeProfiles>
+
   <servers>
     <server>
       <id>sonatype-nexus-snapshots</id>
